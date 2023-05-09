568605b0662731faf9e8cf2b73793b322cd15609
Serifou Moussa <mosesbrown17@icloud.com>
2023-05-09 10:55:16 +0200

    variable age plus if

---
+++ age.php
@@ -0,0 +1,12 @@
+<?php
+
+$age = 20; // variable contenant l'âge
+
+if ($age >= 18) {
+    echo "Je suis majeur";
+} elseif ($age < 18) {
+    echo "Je suis mineur";
+} else {
+    echo "Y a un problème";
+}
+?>
