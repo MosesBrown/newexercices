30c3a00788171b3403fcfe2fdc719bde1178ba1d
Serifou Moussa <mosesbrown17@icloud.com>
2023-05-09 10:41:25 +0200

    tableau

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo ('Hello world HI');
+echo ('Hello world');
+<br>
+
+
+
+// Création du tableau contenant les noms des films
+$films = array("Le Parrain", "La Liste de Schindler", "Forrest Gump", "Le Seigneur des Anneaux", "Inception");
+
+
+foreach ($films as $film) {
+    echo $film . "<br>";
+}
+
+?>
+
+ 
